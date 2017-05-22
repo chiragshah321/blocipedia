262a55fe5f4e63984bc646a195daf3da5cce10c5
Chirag Shah <chiragshah321@gmail.com>
2017-05-22 21:07:03 +0000

    user-story-5-complete

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@
  gem 'bootstrap-sass', '~> 3.3.6'
  gem 'sass-rails', '>= 3.2'
  gem 'devise'
-
+ gem 'pundit'
 
  group :development, :test do
    gem 'byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ActiveRecord::Base
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, 
          :confirmable
+  
+  enum role: [:standard, :premium, :admin]
+  
+  has_one :role
+  
+  after_initialize :init
+  
+  def init
+    self.role ||= 0
+  end
+         
 end
